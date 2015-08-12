bf8460c38a9ccfe4604e1c11b428d57537120e70
Abraham Kuri <kurenn@icalialabs.com>
2015-08-11 19:30:15 -0500

    Refactors view but decorator became more complex

--- presenters/profiler/app/decorators/user_decorator.rb
+++ presenters/profiler/app/decorators/user_decorator.rb
@@ -6,64 +6,96 @@ class UserDecorator
   end
 
   def image_url
-    if user.image_url?
-      user.image_url
+    if user
+      if user.image_url?
+        user.image_url
+      else
+        "http://placehold.it/200x200"
+      end
     else
-      "http://placehold.it/200x200"
+        "http://placehold.it/200x200"
     end
   end
 
   def name
-    if user.name?
-      user.name
+    if user
+      if user.name?
+        user.name
+      else
+        "No name given"
+      end
     else
       "No name given"
     end
   end
 
   def username
-    if user.username?
-      user.username
+    if user
+      if user.username?
+        user.username
+      else
+        "No username given"
+      end
     else
       "No username given"
     end
   end
 
   def email
-    if user.email?
-      user.email
+    if user
+      if user.email?
+        user.email
+      else
+        "No email given"
+      end
     else
       "No email given"
     end
   end
 
   def website
-    if user.website?
-      user.website
+    if user
+      if user.website?
+        user.website
+      else
+        "No website given"
+      end
     else
       "No website given"
     end
   end
 
   def github
-    if user.github?
-      user.github
+    if user
+      if user.github?
+        user.github
+      else
+        "No github given"
+      end
     else
       "No github given"
     end
   end
 
   def twitter
-    if user.twitter?
-      user.twitter
+    if user
+      if user.twitter?
+        user.twitter
+      else
+        "No twitter given"
+      end
     else
       "No twitter given"
     end
   end
 
   def bio
-    if user.bio?
-      user.bio
+    if user
+      if user.bio?
+        user.bio
+      else
+        "No bio given"
+      end
     else
       "No bio given"
     end
